8583773e5f0242d517b62d5eeb2aa59ba86b75aa
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2023-03-29 18:48:55 -0300

    ok

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>Olá, mundo!</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
