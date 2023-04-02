f5f6d704e701022bce17f3370a88ce7ef5c54463
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2023-04-02 02:26:20 -0300

    Refactor do Projeto

--- server/src/server.js
+++ server/src/server.js
@@ -6,19 +6,22 @@ require('dotenv').config();
 
 
 const app = express(); // Cria uma instância do servidor Express
-
 // Configuração de middleware
 app.use(bodyParser.json()); // Adiciona o middleware Body Parser para lidar com as solicitações JSON
 app.use(cors()); // Adiciona o middleware CORS para permitir o acesso a recursos de diferentes origens
+app.use(express.json());
+
 
 // Configuração de rotas
-app.use('/api', routes); // Define o prefixo "/api" para todas as rotas definidas no arquivo "routes.js"
+app.use(routes); // Define o prefixo "/api" para todas as rotas definidas no arquivo "routes.js"
+app.get('/', (req, res) =>{
+    res.send("SERVIDOR ON!");
+}); //
 
-app.use('/', routes);
 
+const port = process.env.SERVER_PORT || 5000; // Define a porta do servidor, lendo a variável de ambiente "PORT" ou usando a porta 5000 por padrão
 
 // Inicialização do servidor
-const port = process.env.PORT || 5000; // Define a porta do servidor, lendo a variável de ambiente "PORT" ou usando a porta 5000 por padrão
-app.listen(port, () => { // Inicia o servidor na porta especificada
+app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
