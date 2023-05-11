b2c772291ab9ee43801656e96b6fbfeeb7b8b007
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2023-05-11 20:19:00 -0300

    ok

--- server/src/database.js
+++ server/src/database.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 const { createClient } = require("@supabase/supabase-js");
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
