a95f1c784b49baf2a784aac55ebab80af1973f56
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2023-05-11 22:12:34 -0300

    cadastrarProduto

--- views/src/components/CadastroProdutoPage/CadastroProduto.js
+++ views/src/components/CadastroProdutoPage/CadastroProduto.js
@@ -1,15 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Header from '../Header/Header';
+import iconeVoltar from './iconeVoltar.png';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 function CadastroProduto() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [selectedOption, setSelectedOption] = useState("opcao1");
+  const handleDateChange = (date) => {
+    setStartDate(date);
+  };
 
+  function handleSelectChange(event) {
+    setSelectedOption(event.target.value);
+  }
 
   return (
     <div>
       <Header />
-      // o conteúdo da sua página aqui
+      <div className="cadastro-produto-wrap">
+        <div className="title-cadastro-voltar" href="/home">
+          <a href="/home" class="home">
+            <img src={iconeVoltar} alt="Ícone da Opção Voltar" className="icone" />
+            CADASTRAR PRODUTO
+          </a>
+        </div>
+      </div>
+
+      <form className="cadastro-produto">
+        <div className='produto-field-form'>
+          <div className='label-produto'>
+            <label htmlFor="produto">Produto</label>
+          </div>
+
+          <input type="text"
+            name="produto"
+            placeholder="Informe o Produto:"
+            required />
+        </div>
+
+        <div className='data-validade-field-form'>
+          <div className='label-data-validade'>
+            <label htmlFor="data-validade">Data de Validade</label>
+          </div>
+
+          <DatePicker
+            selected={startDate}
+            onChange={handleDateChange}
+            showDayMonthYearPicker
+            dateFormat="dd/MM/yyyy" />
+
+          <div className='imagem-field-form'>
+            <div className='label-imagem'>
+              <label htmlFor="imagem">IMAGEM</label>
+
+            </div>
+            <div className='input-imagem'>
+
+            </div>
+            <input type="file" accept="image/*" />
+          </div>
+          <div className='categoria-field-form'>
+            <div className='label-categoria'>
+              <label htmlFor="categoria">CATEGORIA</label>
+            </div>
+            <select value={selectedOption} onChange={handleSelectChange}>
+              <option value="opcao1">Categoria 1</option>
+              <option value="opcao2">Categoria 2</option>
+              <option value="opcao3">Categoria 3</option>
+            </select>
+          </div>
+
+          <div className='button-confirmar'>
+            <button type="submit"
+              className='btn-confirmar'>
+             </button>
+
+          </div>
+        </div>
+      </form>
+
     </div>
+
+
   );
 }
 
