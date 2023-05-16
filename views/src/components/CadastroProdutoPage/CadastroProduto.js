9f4268d7f9c5d8adc20b844ba64946756a4c038d
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2023-05-16 16:01:31 -0300

    teste

--- views/src/components/CadastroProdutoPage/CadastroProduto.js
+++ views/src/components/CadastroProdutoPage/CadastroProduto.js
@@ -28,60 +28,65 @@ function CadastroProduto() {
             CADASTRAR PRODUTO
           </a>
         </div>
-      </div>
+        <form className="cadastro-produto">
+          <div className='produto-field-form'>
+            <div className='label-produto'>
+              <label htmlFor="produto">Produto</label>
+            </div>
 
-      <form className="cadastro-produto">
-        <div className='produto-field-form'>
-          <div className='label-produto'>
-            <label htmlFor="produto">Produto</label>
+            <input type="text"
+              name="produto"
+              placeholder="Informe o Produto:"
+              required />
           </div>
 
-          <input type="text"
-            name="produto"
-            placeholder="Informe o Produto:"
-            required />
-        </div>
+          <div className='data-validade-field-form'>
+            <div className='label-data-validade'>
+              <label htmlFor="data-validade">Data de Validade</label>
+            </div>
 
-        <div className='data-validade-field-form'>
-          <div className='label-data-validade'>
-            <label htmlFor="data-validade">Data de Validade</label>
+            <DatePicker
+              selected={startDate}
+              onChange={handleDateChange}
+              showDayMonthYearPicker
+              dateFormat="dd/MM/yyyy" />
           </div>
-
-          <DatePicker
-            selected={startDate}
-            onChange={handleDateChange}
-            showDayMonthYearPicker
-            dateFormat="dd/MM/yyyy" />
-
+          
           <div className='imagem-field-form'>
             <div className='label-imagem'>
               <label htmlFor="imagem">IMAGEM</label>
 
             </div>
             <div className='input-imagem'>
-
             </div>
-            <input type="file" accept="image/*"  required/>
+            <input type="file" accept="image/*" required />
           </div>
+
           <div className='categoria-field-form'>
             <div className='label-categoria'>
               <label htmlFor="categoria">CATEGORIA</label>
             </div>
-            <select value={selectedOption} onChange={handleSelectChange}  required>
-              <option value="opcao1">Categoria 1</option>
-              <option value="opcao2">Categoria 2</option>
-              <option value="opcao3">Categoria 3</option>
-            </select>
+            <div className='input-categoria'>
+              <select value={selectedOption} onChange={handleSelectChange} required>
+                <option value="opcao1">Categoria 1</option>
+                <option value="opcao2">Categoria 2</option>
+                <option value="opcao3">Categoria 3</option>
+              </select>
+            </div>
           </div>
 
           <div className='button-confirmar'>
             <button type="submit"
               className='btn-confirmar'>
-             </button>
-             <input type="button" value="CONFIRMAR" className="confirmar-button"/>
+            </button>
+            <input type="button" value="CONFIRMAR" className="confirmar-button" />
           </div>
-        </div>
-      </form>
+
+        </form>
+
+      </div>
+
+
 
     </div>
 
